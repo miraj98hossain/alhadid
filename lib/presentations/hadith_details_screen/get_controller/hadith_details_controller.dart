d093610c34596179406d52f99d895aaa0683e4ae
Miraj <miraj09hossain@gmail.com>
2024-07-16 13:21:04 +0600

    loding all the data in controller

--- lib/presentations/hadith_details_screen/get_controller/hadith_details_controller.dart
+++ lib/presentations/hadith_details_screen/get_controller/hadith_details_controller.dart
@@ -1,5 +1,8 @@
 import 'package:alhadid/helpers/database_helper.dart';
+import 'package:alhadid/models/books.dart';
+import 'package:alhadid/models/chapter.dart';
 import 'package:alhadid/models/hadith.dart';
+import 'package:alhadid/models/section.dart';
 import 'package:get/get.dart';
 
 class HadithDetailsController extends GetxController {
@@ -8,19 +11,30 @@ class HadithDetailsController extends GetxController {
   RxBool error = false.obs;
   final DictionaryDataBaseHelper _dbHelper = DictionaryDataBaseHelper();
   RxList<Hadith> hadithList = <Hadith>[].obs;
-
+  RxList<Book> bookList = <Book>[].obs;
+  RxList<Section> sectionList = <Section>[].obs;
+  RxList<Chapter> chapterList = <Chapter>[].obs;
   Future<void> initializeDatabase() async {
     await _dbHelper.init();
     loading.value = true;
-    _loadHadithData();
+    _loadData();
   }
 
-  Future<void> _loadHadithData() async {
+  Future<void> _loadData() async {
     hadithList.value = await _dbHelper.getAllTheHadiths();
-    if (hadithList.isEmpty) {
+    bookList.value = await _dbHelper.getAllTheBooks();
+    chapterList.value = await _dbHelper.getAllTheChapters();
+    sectionList.value = await _dbHelper.getAllTheSections();
+    if (hadithList.isEmpty &&
+        bookList.isEmpty &&
+        chapterList.isEmpty &&
+        sectionList.isEmpty) {
       error.value = true;
       loading.value = false;
-    } else if (hadithList.isNotEmpty) {
+    } else if (hadithList.isNotEmpty &&
+        bookList.isNotEmpty &&
+        chapterList.isNotEmpty &&
+        sectionList.isNotEmpty) {
       success.value = true;
       loading.value = false;
     }
